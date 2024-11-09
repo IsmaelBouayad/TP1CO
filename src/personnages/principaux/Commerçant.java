c4c41b582a3df8bd918e085a3cd85de52560ba5c
admin <admin@Ismael.lan>
2024-11-09 19:06:22 +0100

    init

---
+++ src/personnages/principaux/Commerçant.java
@@ -0,0 +1,19 @@
+package personnages.principaux;
+
+import personnages.Humain;
+
+public class Commerçant extends Humain {
+	
+	public Commerçant(String nom, int argent) {
+		super(nom, argent, "the");
+	}
+	
+	public int seFaireExtorquer() {
+		this.perdreArgent(getArgent());
+		return this.getArgent();
+	}
+	
+	public void recevoir(int n) {
+		this.gagnerArgent(n);
+	}
+}
